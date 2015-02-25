ef02eaddd2929c253a554bbeca9ee1a805bcc0ad
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-02-25 08:00:09 +0700

    Addtion commit login logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
+	has_many :microposts, dependent: :destroy
 	before_save { self.email = email.downcase }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
 	                                                          uniqueness: { case_sensitive: false }
-    validates :password, length: { minimum: 6 }	                                                          
+    validates :password, length: { minimum: 6 }, allow_blank: true	                                                          
     has_secure_password	                                                          
 end
